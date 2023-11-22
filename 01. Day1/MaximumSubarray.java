e594e1cb56f6d51eb53f6aaec123bc52939e41bd
PrernaCodifier <33116849+Prernapundir@users.noreply.github.com>
2023-11-22 12:27:31 +0530

    Create MaximumSubarray.java

---
+++ 01. Day1/MaximumSubarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int curr_sum=0;
+        int ans=Integer.MIN_VALUE;
+
+        for(int i=0;i<nums.length;i++){
+            curr_sum+=nums[i];
+            ans=Math.max(ans,curr_sum);
+            if(curr_sum<0) curr_sum=0;
+        }
+
+        return ans;
+    }
+}
