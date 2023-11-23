3ffdf1a26806aa41585c6761ea5e197e75dec620
PrernaCodifier <33116849+Prernapundir@users.noreply.github.com>
2023-11-23 13:42:55 +0530

    Create TwoSum.java

---
+++ 04. Day04/TwoSum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        int[] ans=new int[2];
+
+        for(int i=0;i<nums.length;i++){
+            int val=target-nums[i];
+            if(map.containsKey(val)){
+                ans[0]=i;
+                ans[1]=map.get(val);
+                break;
+            }else{
+                map.put(nums[i],i);
+            }
+        }
+
+        return ans;
+    }
+}
